850850a2feeba59a3d8306676b6f5d957bd6728b
cade0003 <cade0003@stud.ek.dk>
2026-05-24 18:35:00 +0200

    nummer7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // Siderne bygges på serveren hver gang de besøges, i stedet for på forhånd, hvilket ikke er særligt bæredygtigt da den ellers bygger siderne på forhånd og det bruger ikke så meget strøm.
+  // Siderne bygges på serveren hver gang de besøges, i stedet for på forhånd, hvilket ikke er særligt bæredygtigt da den ellers bygger siderne på forhånd og det bruger ikke så meget strøm.
+  output: "server",
+
+  adapter: netlify()
 });
